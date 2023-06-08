f4e2e991b083c7e642c8c2d8e643f254ba06b4f5
1286Evgen <lopatingenia@rambler.ru>
2023-06-08 14:55:15 -0300

    commits4

--- js/global.js
+++ js/global.js
@@ -1,11 +1,11 @@
-let like = document.querySelector(".fa-heart");
-let plus = document.querySelector(".add_cat_button");
-let block = document.querySelector(".page");
-let addForm = document.querySelector(".form_back");
-let addFormClose = document.querySelector(".form-close");
-let form1 = document.forms.add;
+let like = document.querySelector(".fa-heart");             // создаю переменную для лайка
+let plus = document.querySelector(".add_cat_button");       // создаю переменную кнопки плюс (которая разворачивает форму добавления кота)
+let block = document.querySelector(".page");                // создаю переменную для формы куда добавляются карточки с котами
+let addForm = document.querySelector(".form_back");         // создаю переменную с блоком для формы добавления нового кота
+let addFormClose = document.querySelector(".form-close");   // создаю переменную для кнопки закрытия формы добавления кота
+let form1 = document.forms.add;                             // создаю переменную для формы добавления кота
 
-let cats = localStorage.getItem("cats_data");               // массив в скотами в локальном хранилище
+let cats = localStorage.getItem("cats_data");               // создаю переменную для массива в скотами в локальном хранилище
 
-let user = "1286Evgen";
-let path = `https://cats.petiteweb.dev/api/single/${user}`;
+let user = "1286Evgen";                                     // создаю переменную для имени пользователя
+let path = `https://cats.petiteweb.dev/api/single/${user}`; // создаю переменную, куда записываю адрес удаленного хранилища данных
